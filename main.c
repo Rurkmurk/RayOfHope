19ce23323cf6a7f4f659513ab5160d22d107350c
Rurkmurk <urfin32@gmail.com>
2023-05-23 22:06:55 +0300

    moving&jump

--- main.c
+++ main.c
@@ -4,6 +4,10 @@
 
 #define	HIGH_LEVEL	25
 #define	WIDTH_LEVEL	40
+#define RIGHT 1
+#define LEFT 0
+
+#define EMPTY 51
 
 u32 t=0;
 
@@ -12,6 +16,7 @@ struct
 	u8 x;	
 	u8 y;
 	u8 healt;
+	u8 last_direct;
 }player;
 
 
@@ -66,26 +71,73 @@ void draw_player(u8 map[WIDTH_LEVEL][HIGH_LEVEL])
 			{
 				player.x=4*x;
 				player.y=8*(y-1);
+				map[x][y]=EMPTY;
 				break;
 			}
 
 	
-	set_sprite(0,player.x,player.y,10);
+	set_sprite(0,player.x,player.y,12);
 			
 	swap_screen();
 }
 
+u8 control_player()
+{
+	u8 key;
+	u8 dx=0, dy=0, fire=0;
+	u8 direct=0;
+	
+	vsync();
+	key=joystick();
+	vsync();
+	key=joystick();
+	if(key&JOY_UP)
+	{
+		dy=#0x1;
+	}
+	if(key&JOY_RIGHT)
+	{
+		dx=#0x4;
+	}
+	if(key&JOY_LEFT)
+	{
+		dx=#0x8;
+	}
+	if(key&JOY_DOWN)
+	{
+		dy=#0x2;
+	}
+	if(key&JOY_FIRE)
+	{
+		fire=#0x10;
+	}
+	if(key!=FALSE) t=time();
+	direct=dx+dy+fire;
+	return (direct);
+}
+		
+void player_collision(u8 map[WIDTH_LEVEL][HIGH_LEVEL])
+{
+	if (map[(player.x)/4][player.y/8+2]==EMPTY&&map[(player.x+6)/4][player.y/8+2]==EMPTY)
+	{
+		player.y+=2;
+		set_sprite(0,player.x,player.y,13);
+		swap_screen();
+		player_collision(map);
+	}
+	
+}
 
 void idle()
 {
 	if (t+250>time())
 	{
-		set_sprite(0,player.x,player.y,10);
+		set_sprite(0,player.x,player.y,12);
 		swap_screen();
 	}
 	else if (t+300>time())
 	{
-		set_sprite(0,player.x,player.y,11);
+		set_sprite(0,player.x,player.y,13);
 		swap_screen();
 	}
 	else
@@ -95,20 +147,157 @@ void idle()
 }
 
 
-void player_move (i8 dx, i8 dy)
+void player_move (u8 direct, u8 map[WIDTH_LEVEL][HIGH_LEVEL])
 {
-	player.x+=dx;
-	player.y-=dy;
-	set_sprite(0,player.x,player.y,10);
-	swap_screen();
+	u8 i;
+	
+	if (direct==#0x0) idle();
+	
+	//move right
+	if (direct==#0x4)
+	{
+		i=0;
+		while (control_player()==#0x4)
+		{
+			if (map[(player.x)/4+2][(player.y)/8+0]==EMPTY
+				&&map[(player.x)/4+2][(player.y)/8+1]==EMPTY)
+			{
+				player.x+=2;
+				set_sprite(0,player.x,player.y,i);
+				swap_screen();
+				delay (2);
+				player_collision(map);
+				i++;
+				if (i>3) i=0;
+			}
+			else
+			{
+				set_sprite(0,player.x,player.y,0);
+				swap_screen();
+			}
+		}
+		player.last_direct=RIGHT;
+	}
+
+	//move left
+	if (direct==#0x8)
+	{
+		i=4;
+		while (control_player()==#0x8)
+		{
+			if (map[(player.x)/4][(player.y)/8]==EMPTY
+				&&map[(player.x)/4][(player.y)/8+1]==EMPTY)
+			{
+				player.x-=2;
+				set_sprite(0,player.x,player.y,i);
+				swap_screen();
+				delay (2);
+				player_collision(map);
+				i++;
+				if (i>7) i=4;
+			}
+			else
+			{
+				set_sprite(0,player.x,player.y,4);
+				swap_screen();
+			}
+		}
+		player.last_direct=LEFT;
+	}
+	
+	//jump up
+	if (direct==#0x1)
+	{
+		for (i=0;i<4;i++)
+		{
+			player.y-=2;
+			set_sprite(0,player.x,player.y,13);
+			swap_screen();
+			delay (i/2);
+		}
+		if (map[player.x/4][player.y/8+2]==EMPTY)
+		{
+			player.y+=2;
+			set_sprite(0,player.x,player.y,12);
+			swap_screen();
+		}
+	}
+	//jump right
+	if (direct==#0x5)
+	{
+		for (i=0;i<5;i++)
+		{
+			if (map[(player.x)/4+2][(player.y)/8+1]==EMPTY
+				&&map[(player.x)/4+2][(player.y)/8+2]==EMPTY) player.x+=2;
+			player.y-=2;
+			set_sprite(0,player.x,player.y,10);
+			swap_screen();
+			delay (i);
+		}
+		if (map[player.x/4][player.y/8+2]==EMPTY)
+		{
+			if (map[(player.x)/4+2][(player.y)/8+1]==EMPTY
+				&&map[(player.x)/4+2][(player.y)/8+2]==EMPTY) player.x+=2;
+			player.y+=2;
+			set_sprite(0,player.x,player.y,10);
+			swap_screen();
+			
+		}
+		player.last_direct=RIGHT;
+	}
+	//jump left
+	if (direct==#0x9)
+	{
+		for (i=0;i<5;i++)
+		{
+			if (map[(player.x)/4][(player.y)/8+1]==EMPTY
+				&&map[(player.x)/4][(player.y)/8+2]==EMPTY) player.x-=2;
+			player.y-=2;
+			set_sprite(0,player.x,player.y,11);
+			swap_screen();
+			delay (i);
+		}
+		if (map[player.x/4][player.y/8+2]==EMPTY)
+		{
+			if (map[(player.x)/4][(player.y)/8+1]==EMPTY
+				&&map[(player.x)/4][(player.y)/8+2]==EMPTY) player.x-=2;
+			player.y+=2;
+			set_sprite(0,player.x,player.y,11);
+			swap_screen();
+			
+		}
+		player.last_direct=LEFT;
+	}
+	
+	//fire left
+	if (direct==#0xA)
+	{
+			set_sprite(0,player.x,player.y,9);
+			swap_screen();
+			player.last_direct=LEFT;
+	}
+	//fire right
+	if (direct==#0x6)
+	{
+			set_sprite(0,player.x,player.y,8);
+			swap_screen();
+			player.last_direct=RIGHT;
+	}
+	//down
+	if (direct==#0x2)
+	{
+			if (player.last_direct==RIGHT)
+				set_sprite(0,player.x,player.y,8);
+			else
+				set_sprite(0,player.x,player.y,9);
+			swap_screen();
+	}
 }
 
 
 void main(void)
 {
 	u8 map[WIDTH_LEVEL][HIGH_LEVEL];
-	u8 key;
-	i8 dx=0, dy=0;
 	
 	pal_select(PAL_PLAYER);
 	clear_screen(0);
@@ -121,42 +310,12 @@ void main(void)
 	draw_level(map);
 	
 	draw_player(map);
-	
+
 	
 	while (1)
 	{
-		idle();
-		vsync();
-		key=joystick();
-		if(key&JOY_UP)
-		{
-			dy=1;
-		}
-		if(key&JOY_RIGHT)
-		{
-			dx=1;
-		}
-		if(key&JOY_LEFT)
-		{
-			dx=-1;
-		}
-		if(key&JOY_DOWN)
-		{
-			dy=-1;
-		}
-		if(key&JOY_FIRE)
-		{
-			
-		}
-		if (key!=FALSE)
-		{
-			player_move(dx,dy);
-			dx=0;
-			dy=0;
-			t=time();
-		}
-			
-
+		player_move(control_player(),map);
+		player_collision(map);
 	}
 	
 }
