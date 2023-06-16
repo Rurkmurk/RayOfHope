7ba9f38c4ff3d48aedbe85612033447c63821786
Rurkmurk <124883927+Rurkmurk@users.noreply.github.com>
2023-06-16 15:46:34 +0300

    add timer

--- main.c
+++ main.c
@@ -249,66 +249,74 @@ void update()
 	//player anim
 	set_sprite(0,player.x,player.y,player.frame);
 	
-	//lava anim
-	select_image(IMG_TILE);
-	for (n=0;n<lava_summ;n++)
+		
+	if (t+5<time())
 	{
-		tmp=lava[n].tile[0];
-		lava[n].tile[0]=lava[n].tile[1];
-		lava[n].tile[1]=lava[n].tile[2];
-		lava[n].tile[2]=lava[n].tile[3];
-		lava[n].tile[3]=tmp;
-		
-		tmp=lava[n].tile[4];
-		lava[n].tile[4]=lava[n].tile[5];
-		lava[n].tile[5]=lava[n].tile[6];
-		lava[n].tile[6]=lava[n].tile[7];
-		lava[n].tile[7]=tmp;
-		
-		
-		for (m=0;m<4;m++)
+		//lava anim
+		select_image(IMG_TILE);
+		for (n=0;n<lava_summ;n++)
 		{
-			draw_tile(lava[n].x[m],lava[n].y[0],lava[n].tile[m]);
-			draw_tile(lava[n].x[m],lava[n].y[1],lava[n].tile[m+4]);
-		}
-	}
-	// water anim
-	for (n=0;n<water_summ;n++)
-	{
-		tmp=water[n].tile[0];
-		water[n].tile[0]=water[n].tile[1];
-		water[n].tile[1]=water[n].tile[2];
-		water[n].tile[2]=water[n].tile[3];
-		water[n].tile[3]=tmp;
-		
-		switch (n_frame)
-		{
-			case 0:
-			water[n].tile[4]+=4;
-			water[n].tile[5]+=4;
-			water[n].tile[6]+=4;
-			water[n].tile[7]+=4;
-			break;
-			case 1:
-			water[n].tile[4]-=4;
-			water[n].tile[5]-=4;
-			water[n].tile[6]-=4;
-			water[n].tile[7]-=4;
-			break;
+			tmp=lava[n].tile[0];
+			lava[n].tile[0]=lava[n].tile[1];
+			lava[n].tile[1]=lava[n].tile[2];
+			lava[n].tile[2]=lava[n].tile[3];
+			lava[n].tile[3]=tmp;
+			
+			tmp=lava[n].tile[4];
+			lava[n].tile[4]=lava[n].tile[5];
+			lava[n].tile[5]=lava[n].tile[6];
+			lava[n].tile[6]=lava[n].tile[7];
+			lava[n].tile[7]=tmp;
+			
+			
+			for (m=0;m<4;m++)
+			{
+				draw_tile(lava[n].x[m],lava[n].y[0],lava[n].tile[m]);
+				draw_tile(lava[n].x[m],lava[n].y[1],lava[n].tile[m+4]);
+			}
 		}
 		
-		for (m=0;m<4;m++)
-		{
-			draw_tile(water[n].x[m],water[n].y[0],water[n].tile[m]);
-			draw_tile(water[n].x[m],water[n].y[1],water[n].tile[m+4]);
-		}
-	}	
 	
-	if (n_frame==0) n_frame=1;
-	else n_frame=0;
-
+	
+		// water anim
+		for (n=0;n<water_summ;n++)
+		{
+			tmp=water[n].tile[0];
+			water[n].tile[0]=water[n].tile[1];
+			water[n].tile[1]=water[n].tile[2];
+			water[n].tile[2]=water[n].tile[3];
+			water[n].tile[3]=tmp;
+			
+			switch (n_frame)
+			{
+				case 0:
+				water[n].tile[4]+=4;
+				water[n].tile[5]+=4;
+				water[n].tile[6]+=4;
+				water[n].tile[7]+=4;
+				break;
+				case 1:
+				water[n].tile[4]-=4;
+				water[n].tile[5]-=4;
+				water[n].tile[6]-=4;
+				water[n].tile[7]-=4;
+				break;
+			}
+			
+			for (m=0;m<4;m++)
+			{
+				draw_tile(water[n].x[m],water[n].y[0],water[n].tile[m]);
+				draw_tile(water[n].x[m],water[n].y[1],water[n].tile[m+4]);
+			}
+		}	
+		
+		if (n_frame==0) n_frame=1;
+		else n_frame=0;
+		t=time();
+	}
 	border (13);
 	swap_screen();
+	
 }
 
 
@@ -623,6 +631,7 @@ void player_move (u16 direct)
 void main(void)
 {
 	u8 name[3];
+	t=time();
 	
 	pal_select(PAL_PALETTE0);
 	clear_screen(0);
